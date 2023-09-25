11b914f519aff1eb0ceb30ebb569d2385a795b35
Albin-Joyeux <albin.joyeux@ensta-paris.fr>
2023-09-25 10:22:34 +0200

    2 bubbles

--- bubble-test-app/App.js
+++ bubble-test-app/App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Bubble from "react-bubble-ui";
+
+const bubbles = [
+  {
+    title: "Ma bulle 1",
+    content: "Ceci est le contenu de la bulle 1",
+  },
+  {
+    title: "Ma bulle 2",
+    content: "Ceci est le contenu de la bulle 2",
+  },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {bubbles.map((bubble) => (
+        <View
+          key={bubble.title}
+          style={{ backgroundColor: "red", 
+          width: 100, 
+          height: 100, 
+          borderRadius: 50,
+        }}
+        >
+          <Text>{bubble.title}</Text>
+          <Text>{bubble.content}</Text>
+        </View>
+      ))}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
